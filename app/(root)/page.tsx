837e622d1984f698a8ae91b38018872d925cfaef
ingarbi <104501854+ingarbi@users.noreply.github.com>
2025-03-05 18:02:35 +0300

    Added shadcn ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export const metadata = {
+  title: "Главная",
+}
+
+const HomePage = () => {
+  return  <Button>Home</Button> ;
+}
+ 
+export default HomePage;
