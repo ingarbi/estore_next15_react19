0c148efe8ca19382fd7ed4493b8c4a68338c7373
ingarbi <104501854+ingarbi@users.noreply.github.com>
2025-03-10 22:42:13 +0300

    data-sample added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
-import { Button } from "@/components/ui/button";
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 export const metadata = {
   title: "Главная",
-}
+};
 
 const HomePage = () => {
-  return  <Button>Home</Button> ;
-}
- 
+  return (
+    <ProductList
+      data={sampleData.products}
+      title="Новые поступления"
+      limit={4}
+    />
+  );
+};
+
 export default HomePage;
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,27 @@
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title?: string;
+  limit?: number;
+}) => {
+  const limitedData = limit ? data.slice(0, limit) : data;
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {limitedData.map((product: any) => (
+            <div key={product.id}>{product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div>Нет товаров</div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
